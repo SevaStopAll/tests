365df409eefc80ba8afb900464956db6df81ae99
SevaStopAll <sevastopallrh@yandex.ru>
2023-05-11 10:14:45 +0400

    Added two solutions from HackerRank.

---
+++ SubstringComparisons.java
@@ -0,0 +1,25 @@
+package ru.job4j.order;
+
+public class SubstringComparisons {
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "zzz";
+        String largest = "";
+        int beginIndex = 0;
+        String res = "";
+        for(int i = beginIndex; i < s.length() - k+1; i++) {
+            res = s.substring(i, i + k);
+            if (smallest.compareTo(res) > 0) {
+                smallest = res;
+            }
+            if (largest.compareTo(res) < 0) {
+                largest = res;
+            }
+        }
+
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SubstringComparisons.getSmallestAndLargest("welcometojava", 3));
+    }
+}
---
+++ isPalindrome.java
@@ -0,0 +1,19 @@
+package ru.job4j.order;
+
+import java.util.Scanner;
+
+public class isPalindrome {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        String a = sc.next();
+        String result = "Yes";
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != a.charAt(a.length() - 1 - i)) {
+                result = "No";
+            }
+        }
+        System.out.println(result);
+
+    }
+}
