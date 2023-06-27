281d3313e690971f1ca83b58cddaac59b6608443
SevaStopAll <sevastopallrh@yandex.ru>
2023-06-27 08:08:17 +0400

    added AddTwoNumbers from Leetcode

--- ru/tests/AddTwoNumbers/Solution.java
+++ ru/tests/AddTwoNumbers/Solution.java
@@ -1,39 +1,30 @@
-package AddTwoNumbers;
+package ru.job4j.order.ru.tests.AddTwoNumbers;
 
 import ru.job4j.order.ru.tests.ListNode;
 
-import java.util.HashMap;
-import java.util.Map;
-
-
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
-        StringBuilder builder = new StringBuilder();
-        while (l1 != null) {
-            builder.append(l1.val);
-            l1 = l1.next;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry == 1)  {
+            int sum = 0;
+            if (l1 != null) {
+                sum +=l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            curr.next = node;
+            curr = curr.next;
         }
-        StringBuilder builder2 = new StringBuilder();
-        while (l2 != null) {
-            builder2.append(l2.val);
-            l2 = l2.next;
+        return dummy.next;
         }
-
-        /// Переполнение типа Long. Надо сделать в массиве, видимо. Или в мапе.
-        Map<Integer, Integer> firstNumber = new HashMap<>();
-        Map<Integer, Integer> secondNumber = new HashMap<>();
-
-        String targetNumber = String.valueOf(Long.parseLong(builder.reverse().toString()) + Long.parseLong(builder2.reverse().toString()));
-        char[] ch = targetNumber.toCharArray();
-        ListNode first = new ListNode(Character.digit(ch[ch.length - 1], 10));
-        ListNode second = first;
-        for (int i = ch.length - 2; i >= 0; i--) {
-            second.next = new ListNode(Character.digit(ch[i], 10));
-            second = second.next;
-        }
-        return first;
-    }
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode();
