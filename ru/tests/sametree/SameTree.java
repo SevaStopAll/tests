03046aad63b9985766d3fb7a9b529243f359f8fb
SevaStopAll <Sevastopallrh@yandex.ru>
2023-07-04 14:01:20 +0400

    Added ClimbingStairs, MergeSortedArray and SameTree tasked

--- ru/tests/sametree/SameTree.java
+++ ru/tests/sametree/SameTree.java
@@ -2,13 +2,26 @@ package ru.job4j.order.ru.tests.sametree;
 
 public class SameTree {
 
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    public static boolean isSameTree(ru.job4j.order.ru.tests.sametree.TreeNode p, ru.job4j.order.ru.tests.sametree.TreeNode q) {
         boolean result = true;
-            if (p.val != q.val) {
-                return false;
-            }
-            isSameTree(p.left, q.left);
-            isSameTree(p.right, q.right);
-            return result;
+        while (p != null && q != null) {
+        if (p.val != q.val) {
+            return false;
+        }
+        isSameTree(p.left, q.left);
+        isSameTree(p.right, q.right);
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ru.job4j.order.ru.tests.sametree.TreeNode p = new ru.job4j.order.ru.tests.sametree.TreeNode(1);
+        ru.job4j.order.ru.tests.sametree.TreeNode q = new ru.job4j.order.ru.tests.sametree.TreeNode(1);
+        p.left = new ru.job4j.order.ru.tests.sametree.TreeNode(2);
+        q.left = new ru.job4j.order.ru.tests.sametree.TreeNode(2);
+        p.right = new ru.job4j.order.ru.tests.sametree.TreeNode(3);
+        q.right = new ru.job4j.order.ru.tests.sametree.TreeNode(3);
+        SameTree.isSameTree(p, q);
     }
 }
